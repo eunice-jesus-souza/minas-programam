1b74e040f250202aa0717161d59d7225efa87083
Eunice Souza <ejs.12@hotmail.com>
2022-10-26 20:28:41 -0300

    Atividade 5 par e impar

---
+++ parimpar5.js
@@ -0,0 +1,16 @@
+// Crie um programa que verifique se um determinado número é impar ou par!
+// Caso seja ímpar retorne "O número X é ímpar"
+// Caso seja par retorne "O número X é ímpar"
+
+// Dica do sucesso: todo número par é divisível por 2.
+
+let number1 = 0
+let number2 = 1
+
+if(number1 % 2 == 0 == number2 == 1 ){
+    console.log(`O numero ${0} é par, e o numbero ${1} é impar`)
+}else if (number1 % 2 == 0 == number2 == 1){
+    console.log(`O numero ${0} é impar, e o numbero ${1} é par`)
+}else{
+
+}

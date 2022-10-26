eac60f28b5105fee953c79091d44dc78a7fac9e6
Eunice Souza <ejs.12@hotmail.com>
2022-10-26 13:37:53 -0300

    Aula 3

---
+++ porcentagem3.js
@@ -0,0 +1,19 @@
+// Uma comerciante comprou alguns produtos e quer revender com lucro de 45% se o valor da compra for menor que 20 reais. 
+//Caso contrário, o lucro será de 30%. O valor deve ser passado para o programa que deverá retornar o valor da venda.
+
+// Exemplo: 
+// produto: R$ 18,00 -> 18 * 45% -> R$ 26,10
+// produto: R$ 21,00 -> 21 * 30% -> R$ 27,30
+// let valorProduto= 18;
+
+let valorCompra = 18 * .45 / 26.10
+let lucroProduto = 21 * .30 / 27.30
+let valorProduto = 18 
+
+if (valorCompra > lucroProduto){
+    console.log (`O valor da compra é ${18}`)
+}else if(lucroProduto){
+  console.log (`O valor da venda é maior que ${20}`)
+}else {
+
+}
